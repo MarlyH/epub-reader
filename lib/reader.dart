bb43934888d5c3bb4e6e68f97439713b2431795b
MarlyH <161419386+MarlyH@users.noreply.github.com>
2026-02-01 21:44:24 +1300

    Crudely display EPUB chapters as HTML
    -chapters/subchapters are flattened with recursive method
    - chapters are joined together into one big gross html string for displaying

--- lib/reader.dart
+++ lib/reader.dart
@@ -2,6 +2,8 @@ import 'dart:typed_data';
 import 'package:epub_reader/parseEpub.dart';
 import 'package:epubx/epubx.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter/widgets.dart' as flutter;
 
 class Reader extends StatelessWidget {
   final Uint8List bytes;
@@ -22,12 +24,59 @@ class Reader extends StatelessWidget {
           );
         }
 
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(child: SelectableText('Error: ${snapshot.error}')),
+          );
+        }
+
         final book = snapshot.data!;
+        final chapters = getFlattenedChapters(book);
+
         return Scaffold(
           appBar: AppBar(title: Text(book.Title ?? 'Unknown Title')),
-          body: Center(child: Text('book loaded')),
+          body: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Html(
+              data: buildFullHtml(chapters),
+            ),
+          ),
         );
+
       },
     );
+  }
+
+  // temporary solution to build entire HTML content from all chapters
+  String buildFullHtml(List<EpubChapter> chapters) {
+    final parts = <String>[];
+
+    for (final chapter in chapters) {
+      final html = chapter.HtmlContent;
+      if (html == null || html.isEmpty) continue;
+      parts.add(html);
+    }
+
+    return parts.join();
+  }
+
+
+  // flatten any nested sub-chapters into a single list
+  List<EpubChapter> getFlattenedChapters(EpubBook book) {
+    List<EpubChapter> result = [];
+
+    void recurse(List<EpubChapter>? chapters) {
+      if (chapters == null) return; // base case
+
+      for (var chapter in chapters) {
+        result.add(chapter); // add the current chapter
+        if (chapter.SubChapters!.isNotEmpty) {
+          recurse(chapter.SubChapters); // add nested chapters recursively
+        }
+      }
+    }
+
+    recurse(book.Chapters);
+    return result;
   }
 }
